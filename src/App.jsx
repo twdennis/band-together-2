5c3be4be3846e04c4f9d139077c898f61b22a423
Tom <131278560+twdennis@users.noreply.github.com>
2024-06-18 17:00:44 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,34 @@ import Dashboard from "./features/dashboard/Dashboard";
 import Calendar from "./features/calendar/Calendar";
 import Map from "./features/map/Map";
 import NewEvent from "./features/event/NewEvent";
+import Event from "./features/event/Event";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/calendar",
-    element: <Calendar />,
-  },
-  {
-    path: "/map",
-    element: <Map />,
-  },
-  {
-    path: "/event/new",
-    element: <NewEvent />,
-  },
-  {
-    path: "/event/eventId",
-    elemetn: <NewEvent />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/calendar",
+        element: <Calendar />,
+      },
+      {
+        path: "/map",
+        element: <Map />,
+      },
+      {
+        path: "/event/new",
+        element: <NewEvent />,
+      },
+      {
+        path: "/event/:eventId",
+        element: <Event />,
+      },
+    ],
   },
 ]);
 
--- src/features/event/NewEvent.jsx
+++ src/features/event/NewEvent.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function NewEvent() {
-  return <div>New Event</div>;
+  return (
+    <div>
+      <Link to="/">← Back to dashboard</Link>
+      <h1>New Event</h1>
+    </div>
+  );
 }
 
 export default NewEvent;
