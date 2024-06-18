9600ebb7b2ad37c92a5cbe9f1f49788eb581ad00
Tom <131278560+twdennis@users.noreply.github.com>
2024-06-18 17:15:46 +0100

    created apiUsers.js and pasted api details into file

---
+++ src/services/apiUsers.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://ophkxltlzhasmnraaonk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGt4bHRsemhhc21ucmFhb25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyODc2MzksImV4cCI6MjAzMzg2MzYzOX0.yvKe0DGl9zJbQqpJummmbmrdQEio1Nb-4fYR3tcqYVw"
+);
